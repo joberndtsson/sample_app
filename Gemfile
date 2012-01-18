43d6064739dc718d9d61f1e5de264349ee30a0d5
joberndtsson <fooclouds@yahoo.com>
2012-01-18 10:42:40 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+
+gem 'sqlite3'
+
+group :development do
+gem 'rspec-rails', '2.8.1'
+end
+
+group :test do
+gem 'rspec', '2.8.0'
+end
+
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9aa49c965f8d5c6770607c701c76df4a9a38d6ead517aed2d4b7ce0c42e086c31d8e222d56057f7eb6a48c6bdab5acb8077b928cc25b4dc773aa8d440b7c8954'
